fa10a61732808dd939e9010b7adef66c88fa1a6f
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2023-05-30 01:10:15 +0200

    working draft

---
+++ main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 12:07:14 2023
+
+@author: Alessandro
+"""
+import sys
+from physical import Directory
+from physical import CSV
+from physical import Filesystem
+from datetime import datetime
+import logging
+
+class Main:
+    """Main class"""
+    
+        
+    def projects(args):
+        """Return the list of projects"""
+        Filesystem( args[2], 
+                   CSV (
+                       datetime.now(), 
+                       Directory( args[1] ).xml() 
+                       )
+                   ).file()
+        
+        
+    if __name__ == '__main__':
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug('Begin')
+        args = sys.argv
+        if( 3 != len(args) ) :
+            raise Exception("Usage: main.py input_path output_path")
+        projects(args)
+        logging.info('End')
